f55b056fc89b42f810e682b3c12e94a2654204fd
mindy168 <52755054+mindy168@users.noreply.github.com>
2019-07-11 08:51:44 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,1 +1,21 @@
-window.alert("This page is constantly trying to be accessible for all! :)");
+function Data() {
+  document.getElementById("demo").innerHTML="This website sucks watch out";
+}
+
+function funFact() {
+  document.getElementById("demo2").innerHTML="Grande performing on her Sweetner tour";
+}
+
+function funFact2() {
+  document.getElementById("demo3").innerHTML="The Current Members of Paramore - Left: Taylor York, Middle: Hayley Williams, Right: Zac Farro";
+}
+
+function funFact3() {
+  document.getElementById("demo4").innerHTML="Jessie J singing (wow did NOT expect that)";
+}
+
+function expandPic() {
+    document.getElementById("jessieJ").innerHTML="Just kidding i don't know how to do that";
+  }
+
+
